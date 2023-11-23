bb6a99be46aee7755b7bbe9ef2921f54d5ec32ee
FrederikBA <frederikbandersen98@gmail.com>
2023-11-23 14:44:06 +0100

    test run

--- src/Auth.Web/Program.cs
+++ src/Auth.Web/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World! from Auth.Web!");
+app.MapGet("/", () => "This has changed!");
 
 app.Run();
