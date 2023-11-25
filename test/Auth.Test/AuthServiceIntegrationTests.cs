436361d70b013df1fd1b10fb6f0afbb56ce241d5
FrederikBA <frederikbandersen98@gmail.com>
2023-11-25 13:21:03 +0100

    register account integration tests

---
+++ test/Auth.Test/AuthServiceIntegrationTests.cs
@@ -0,0 +1,44 @@
+using Auth.Web.Data;
+using Auth.Web.Entities;
+using Auth.Web.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MTOGO.Web.Models.Dto.Auth;
+
+namespace Auth.Test;
+
+public class AuthServiceIntegrationTests
+{
+    [Fact]
+    public async Task RegisterCustomerAsync_ShouldSaveUserToDatabase()
+    {
+        // Arrange
+        var serviceProvider = new ServiceCollection()
+            .AddDbContext<UserContext>(options => options.UseInMemoryDatabase(databaseName: "TestDB"))
+            .BuildServiceProvider();
+
+        var dbContext = serviceProvider.GetRequiredService<UserContext>();
+        await dbContext.Database.EnsureCreatedAsync();
+
+        var userRepository = new EfRepository<User>(dbContext);
+        var authService = new AuthService(userRepository);
+
+        var dto = new RegisterDto
+        {
+            Email = "test@example.com",
+            Password = "password"
+        };
+
+        // Act
+        var result = await authService.RegisterCustomerAsync(dto);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(dto.Email, result.Email);
+
+        // Check if the user is saved in the in-memory database
+        var savedUser = await dbContext.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+        Assert.NotNull(savedUser);
+        Assert.Equal(dto.Email, savedUser.Email);
+    }
+}
--- test/Auth.Test/AuthServiceUnitTests.cs
+++ test/Auth.Test/AuthServiceUnitTests.cs
@@ -1,77 +1,77 @@
-using Auth.Web.Entities;
-using Auth.Web.Interfaces.DomainServices;
-using Auth.Web.Interfaces.Repositories;
-using Auth.Web.Services;
-using Moq;
-using MTOGO.Web.Models.Dto.Auth;
+    using Auth.Web.Entities;
+    using Auth.Web.Interfaces.DomainServices;
+    using Auth.Web.Interfaces.Repositories;
+    using Auth.Web.Services;
+    using Moq;
+    using MTOGO.Web.Models.Dto.Auth;
 
-namespace Auth.Test;
+    namespace Auth.Test;
 
-public class AuthServiceUnitTests
-{
-    private readonly IAuthService _authService;
-    private readonly Mock<IRepository<User>> _userRepositoryMock = new();
+        public class AuthServiceUnitTests
+        {
+            private readonly IAuthService _authService;
+            private readonly Mock<IRepository<User>> _userRepositoryMock = new();
 
-    public AuthServiceUnitTests()
-    {
-        _authService = new AuthService(_userRepositoryMock.Object);
+            public AuthServiceUnitTests()
+            {
+                _authService = new AuthService(_userRepositoryMock.Object);
+            }
+
+        [Fact]
+        public async Task RegisterCustomerAsync_ShouldRegisterUserWithCustomerRole()
+        {
+            //Arrange
+            var dto = new RegisterDto
+            {
+                Email = "test@example.com",
+                Password = "password"
+            };
+
+            //Setup the mock
+            var user = new User
+            {
+                Id = 1,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                RoleId = 1 //Customer
+            };
+
+            //Assure that the repository returns an empty list rather than null
+            _userRepositoryMock.Setup(x => x.ListAsync(new CancellationToken()))
+                .ReturnsAsync(new List<User>());
+
+            //Act
+            var result = await _authService.RegisterCustomerAsync(dto);
+
+            //Assert
+            Assert.Equal(1, result.RoleId); //Is customer role
+            Assert.Equal(dto.Email, result.Email); //Email is correct
+        }
+
+        [Fact]
+        public async Task RegisterCustomerAsync_ShouldThrowExceptionIfEmailAlreadyExists()
+        {
+            //Arrange
+            var dto = new RegisterDto
+            {
+                Email = "test@example.com",
+                Password = "password"
+            };
+            
+            //Setup the mock
+            var user = new User
+            {
+                Id = 1,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                RoleId = 1 //Customer
+            };
+            
+            //Assure that the repository returns a list with one user
+            _userRepositoryMock.Setup(x => x.ListAsync(new CancellationToken()))
+                .ReturnsAsync(new List<User> {user});
+
+            //Act + Assert
+            await Assert.ThrowsAsync<Exception>(() => _authService.RegisterCustomerAsync(dto));
+        }
     }
-
-    [Fact]
-    public async Task RegisterCustomerAsync_ShouldRegisterUserWithCustomerRole()
-    {
-        //Arrange
-        var dto = new RegisterDto
-        {
-            Email = "test@example.com",
-            Password = "password"
-        };
-
-        //Setup the mock
-        var user = new User
-        {
-            Id = 1,
-            Email = dto.Email,
-            Password = BCrypt.Net.BCrypt.HashPassword(dto.Password),
-            RoleId = 1 //Customer
-        };
-
-        //Assure that the repository returns an empty list rather than null
-        _userRepositoryMock.Setup(x => x.ListAsync(new CancellationToken()))
-            .ReturnsAsync(new List<User>());
-
-        //Act
-        var result = await _authService.RegisterCustomerAsync(dto);
-
-        //Assert
-        Assert.Equal(1, result.RoleId); //Is customer role
-        Assert.Equal(dto.Email, result.Email); //Email is correct
-    }
-
-    [Fact]
-    public async Task RegisterCustomerAsync_ShouldThrowExceptionIfEmailAlreadyExists()
-    {
-        //Arrange
-        var dto = new RegisterDto
-        {
-            Email = "test@example.com",
-            Password = "password"
-        };
-        
-        //Setup the mock
-        var user = new User
-        {
-            Id = 1,
-            Email = dto.Email,
-            Password = BCrypt.Net.BCrypt.HashPassword(dto.Password),
-            RoleId = 1 //Customer
-        };
-        
-        //Assure that the repository returns a list with one user
-        _userRepositoryMock.Setup(x => x.ListAsync(new CancellationToken()))
-            .ReturnsAsync(new List<User> {user});
-
-        //Act + Assert
-        await Assert.ThrowsAsync<Exception>(() => _authService.RegisterCustomerAsync(dto));
-    }
-}
